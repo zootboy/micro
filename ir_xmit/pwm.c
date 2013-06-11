bf9d108b5b2aa7921d0b2f75d9954e5d4e455e94
zootboy <zootboysean@gmail.com>
2013-06-11 16:52:16 -0400

    Added infrared transmitter code.

--- ir_xmit/pwm.c
+++ ir_xmit/pwm.c
@@ -3,7 +3,8 @@
 /* Include useful pre-defined functions */
 #include <avr/io.h>
 #include <avr/interrupt.h>   
-#define F_CPU 8000000   
+#define F_CPU 9600000   
+#include <util/delay.h>
 
 // Target frequency: 38kHz
 
@@ -12,10 +13,19 @@ int main(void)
 	// Timer0 settings.
 	TCCR0A = (1<<COM0A0)|(1<<WGM01); // Toggle OC0A on compare.
 	TCCR0B = (1<<CS00); // Enable system clock to timer, no prescale.
-	OCR0A = 211;
+	OCR0A = 245/2;
+	DDRB = (1<<0); // PB0 = output
 
+	uint8_t i;
+		
 	while(1)
 	{
-		asm("nop");	
+		i=20; // ms delay
+		while(i)
+		{
+			_delay_ms(1);
+			i--;
+		}
+		DDRB ^= (1<<0);
     }
 }
